cf0296bb07bbb99f1edbbeeab794be6bd12d727b
Somvit09 <developer.09.sam@gmail.com>
2023-11-25 12:14:45 +0530

    changed hosts

--- carzone/settings.py
+++ carzone/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'carzone-main-sam.herokuapp.com', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'carzone-deployment-production.up.railway.app', ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
